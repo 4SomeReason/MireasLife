5583540c4636092b8cbfb12e87cd13102ebade7a
Semechka <115107268+utsushka@users.noreply.github.com>
2024-01-19 14:31:13 +0300

    Failed commit reroll

--- Mireas/Menu/Assets/MainManu.cs
+++ Mireas/Menu/Assets/MainManu.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 
 public class MainManu : MonoBehaviour
