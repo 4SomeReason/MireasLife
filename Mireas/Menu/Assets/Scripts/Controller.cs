b74d372bc1d83e645cd922541d31c99992e1e5a9
Semechka <115107268+utsushka@users.noreply.github.com>
2024-03-24 22:20:43 +0300

    Added animation to player

--- Mireas/Menu/Assets/Scripts/Controller.cs
+++ Mireas/Menu/Assets/Scripts/Controller.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     public float speed = 300.0f;
+    private bool facingRight = true;
     private Rigidbody2D _body;
+    private Animator animator;
 
     void Start()
     {
         _body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,5 +23,68 @@ public class Controller : MonoBehaviour
         float deltaY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         Vector2 movement2 = new Vector2(_body.velocity.x, deltaY);
         _body.velocity = movement2;
+        PlayAnimation(deltaX, deltaY);
+    }
+
+    void StopAnimation()
+    {
+        animator.SetBool("IsDown", false);
+        animator.SetBool("IsUp", false);
+        animator.SetBool("IsLeft", false);
+        animator.SetBool("IsRight", false);
+    }
+
+    void PlayAnimation(float moveX, float moveY)
+    {
+        if (moveX == 0 && moveY == 0)
+        {
+            StopAnimation();
+        }
+        else
+        {
+            if (moveY > 0 && moveX == 0)
+            {
+                animator.SetBool("IsUp", true);
+                animator.SetBool("IsDown", false);
+                animator.SetBool("IsLeft", false);
+                animator.SetBool("IsRight", false);
+            }
+            else
+            {
+                if (moveY < 0 && moveX == 0)
+                {
+                    animator.SetBool("IsUp", false);
+                    animator.SetBool("IsDown", true);
+                    animator.SetBool("IsLeft", false);
+                    animator.SetBool("IsRight", false);
+                }
+                else
+                {
+                    if (moveX > 0)
+                    {
+                        animator.SetBool("IsUp", false);
+                        animator.SetBool("IsDown", false);
+                        animator.SetBool("IsLeft", false);
+                        animator.SetBool("IsRight", true);
+                    }
+                    else
+                    {
+                        animator.SetBool("IsUp", false);
+                        animator.SetBool("IsDown", false);
+                        animator.SetBool("IsLeft", true);
+                        animator.SetBool("IsRight", false);
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void horizFlip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
